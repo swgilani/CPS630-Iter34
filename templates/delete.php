d5f980c5cff1e62560d029afb316c19118d3cf0c
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-04-14 23:08:33 -0400

    Only finished delete rn

---
+++ templates/delete.php
@@ -0,0 +1,39 @@
+<?php
+  include "../db/dbc.php";
+
+  if ($_GET['table'] == "car_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE carID = " .$_GET['condition'];
+  }
+  if ($_GET['table'] == "coffee_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE coffeeID = " .$_GET['condition'];
+  }
+  if ($_GET['table'] == "flower_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE flowerID = " .$_GET['condition'];
+  }
+  if ($_GET['table'] == "order_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE order_ID = " .$_GET['condition'];
+  }
+  if ($_GET['table'] == "user_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE userID = " .$_GET['condition'];
+  }
+  if ($_GET['table'] == "trip_table")
+  {
+    $sql = "DELETE FROM " .$_GET['table']. " WHERE tripID = " .$_GET['condition'];
+  }
+  //success or fail
+  if($dbc->query($sql) === TRUE)
+  {
+    $dbc -> close();
+    header("location:maintain.php");
+    exit;
+  }
+  else
+  {
+    echo "ERROR: Could not able to execute $sql. ";
+  }
+?>
