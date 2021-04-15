9fdc6460d0144f83beade414cae2505bb830ed2a
Aaron Ooi <aaron.ooi@ryerson.ca>
2021-04-15 12:15:59 -0400

    Browser part

--- templates/home.php
+++ templates/home.php
@@ -1,2 +1,31 @@
 <h1>Plan for Smart Services</h1>
 <img src="/Iter34/img/logo.png" class="center">
+<?php
+$arr_browsers = ["Opera", "Edg", "Chrome", "Safari", "Firefox", "MSIE", "Trident"];
+ 
+$agent = $_SERVER['HTTP_USER_AGENT'];
+ 
+$user_browser = '';
+foreach ($arr_browsers as $browser) {
+    if (strpos($agent, $browser) !== false) {
+        $user_browser = $browser;
+        break;
+    }
+}
+
+switch ($user_browser) {
+    case 'MSIE':
+        $user_browser = 'Internet Explorer';
+        break;
+
+    case 'Trident':
+        $user_browser = 'Internet Explorer';
+        break;
+
+    case 'Edg':
+        $user_browser = 'Microsoft Edge';
+        break;
+}
+
+echo "You are using ".$user_browser." browser";
+?>
